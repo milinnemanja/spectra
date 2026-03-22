086306d6b826bb10de92525ea32a89e6de2e4ac7
milinnemanja <nemanjamilin.contact@gmail.com>
2026-03-22 11:19:32 +0100

    FINAL

--- src/App.tsx
+++ src/App.tsx
@@ -175,9 +175,9 @@ export default function App() {
   const [lang, setLang] = useState<Language>('sr');
   const [projects, setProjects] = useState<Project[]>(PROJECTS);
   const [siteSettings, setSiteSettings] = useState<any>({
-    featuresImageUrl: '/Images/features.jpg',
-    aboutImageUrl: '/Images/about.jpg',
-    ownerImageUrl: '/Images/owner.jpg',
+    featuresImageUrl: '/Images/dvoristemain.jpg',
+    aboutImageUrl: '/Images/exteriorpool.jpg',
+    ownerImageUrl: '/Images/owner.jpeg',
     logoImageUrl: ''
   });
   const t = translations[lang];
